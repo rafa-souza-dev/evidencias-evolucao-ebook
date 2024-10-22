54a6f8fba17dd51eb1b5e7ecb6e114098dc8d362
Rafael Souza <rafaeljosegomes25@gmail.com>
2024-10-22 19:56:38 -0300

    chore: create all scenes files

---
+++ contracapa.lua
@@ -0,0 +1,28 @@
+local composer = require "composer"
+
+local scene = composer.newScene()
+
+function scene:create(event)
+    local title = display.newText({
+        text = "Contracapa",
+        x = CONSTANTS.width / 2,
+        y = 100,
+        font = native.systemFontBold,
+        fontSize = 32
+    })
+
+    local objects = self.view
+
+    objects:insert(title)
+end
+
+function scene:show(event)
+    local currentScene = composer.getSceneName("current")
+
+    renderPreviousButton(currentScene)
+end
+
+scene:addEventListener("create", scene)
+scene:addEventListener("show", scene)
+
+return scene
