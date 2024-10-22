d01c832ef8fe7d24a9c9a1098d5f775518183c3c
Rafael Souza <rafaeljosegomes25@gmail.com>
2024-10-22 20:42:34 -0300

    feat: add largeButton

--- contracapa.lua
+++ contracapa.lua
@@ -2,6 +2,28 @@ local composer = require "composer"
 
 local scene = composer.newScene()
 
+local function criarBotaoLarge()
+    local largura = 388
+    local altura = 62
+    local botao = display.newRoundedRect(0, 0, largura, altura, 12)
+
+    botao:setFillColor(0.459, 0.4, 0.263)
+
+    local textoBotao = display.newText({
+        text = "Voltar ao início",
+        x = botao.x,
+        y = botao.y,
+        font = native.systemFontBold,
+        fontSize = 24
+    })
+
+    local grupoBotao = display.newGroup()
+    grupoBotao:insert(botao)
+    grupoBotao:insert(textoBotao)
+
+    return grupoBotao
+end
+
 function scene:create(event)
     local objects = self.view
 
@@ -13,6 +35,10 @@ function scene:create(event)
         fontSize = 32
     })
 
+    local largeButton = criarBotaoLarge()
+    largeButton.y = CONSTANTS.height - 100
+    largeButton.x = CONSTANTS.width / 2
+
     local previousButton = display.newImage(
         "previous.png",
         60,
@@ -23,8 +49,15 @@ function scene:create(event)
         composer.gotoScene("pagina5", "fade")
     end)
 
+    local function onLargeButtonTap(event)
+        composer.gotoScene("capa", "fade")
+    end
+
+    largeButton:addEventListener("tap", onLargeButtonTap)
+
     objects:insert(title)
     objects:insert(previousButton)
+    objects:insert(largeButton)
 end
 
 scene:addEventListener("create", scene)
