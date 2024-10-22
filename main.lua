4f9fea257cadba6885538d6af5d2c20555f2394f
Rafael Souza <rafaeljosegomes25@gmail.com>
2024-10-22 20:34:54 -0300

    remove unused fn

--- main.lua
+++ main.lua
@@ -4,46 +4,6 @@ function corDecimal(cor)
     return cor / 255
 end
 
-local scenePreviousNavigation = {
-    ["capa"] = nil,
-    ["pagina2"] = "capa",
-    ["pagina3"] = "pagina2",
-    ["pagina4"] = "pagina3",
-    ["pagina5"] = "pagina4",
-    ["contracapa"] = "pagina5"
-}
-
-function renderPreviousButton(currentScene)
-    local previousScene = scenePreviousNavigation[currentScene]
-
-    if previousScene == nil then
-        if PREVIOUS_BUTTON then
-            PREVIOUS_BUTTON.isVisible = false
-        end
-        return
-    end
-
-    if not PREVIOUS_BUTTON then
-        PREVIOUS_BUTTON = display.newImage(
-            "previous.png",
-            50,
-            CONSTANTS.height - 75
-        )
-    end
-
-    PREVIOUS_BUTTON.isVisible = true
-
-    local function goToScene(event)
-        if event.phase == "ended" or event.phase == "cancelled" then
-            composer.gotoScene(previousScene)
-            return true
-        end
-    end
-
-    PREVIOUS_BUTTON:removeEventListener("touch", goToScene)
-    PREVIOUS_BUTTON:addEventListener("touch", goToScene)
-end
-
 display.setDefault("background", corDecimal(145), corDecimal(135), corDecimal(111))
 display.setStatusBar(display.HiddenStatusBar)
 
