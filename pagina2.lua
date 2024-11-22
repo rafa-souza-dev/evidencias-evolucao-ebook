208b59608801a157a0beee6af764d24a2fc92dc3
Rafael Souza <rafaeljosegomes25@gmail.com>
2024-11-22 19:55:15 -0300

    pagina2: insert texts

--- pagina2.lua
+++ pagina2.lua
@@ -37,12 +37,38 @@ local soundControlGroup = SoundControl.new({
 function scene:create(event)
     local objects = self.view
 
-    local title = display.newText({
+    local pageNumber = display.newText({
         text = "Página 2",
-        x = CONSTANTS.width / 2,
+        x = 50,
+        y = 24,
+        fontSize = 14
+    })
+
+    local title = display.newText({
+        text = "Evidências da Evolução - Introdução",
+        x = 300,
         y = 100,
         font = native.systemFontBold,
-        fontSize = 32
+        fontSize = 32,
+        align = "left"
+    })
+
+    local content = display.newText({
+        text = [[
+A teoria da evolução, proposta inicialmente por Charles Darwin no século XIX, sugere que todas as espécies de organismos vivos descendem de um ancestral comum. A evolução ocorre através de processos como seleção natural, onde as características que favorecem a sobrevivência são mais prováveis de serem passadas para as gerações seguintes. Ao longo do tempo, isso resulta em mudanças graduais e no desenvolvimento de novas espécies. A evolução é um pilar da biologia moderna, ajudando a explicar a diversidade da vida na Terra. 
+Darwin observou que, dentro de uma espécie, há uma variação natural — ou seja, os indivíduos não são idênticos entre si. Algumas dessas variações são mais vantajosas para a sobrevivência e a reprodução, dependendo do ambiente em que os indivíduos vivem. Aqueles com características vantajosas têm mais chances de sobreviver e passar essas características para os descendentes. Este processo, conhecido como seleção natural, é o principal mecanismo da evolução.
+A seleção natural é o processo pelo qual organismos com características mais adequadas ao ambiente têm maior probabilidade de sobreviver e se reproduzir, enquanto aqueles menos adaptados tendem a desaparecer com o tempo. Essas características vantajosas são chamadas de adaptações, e podem ser físicas (como pelagem espessa em animais que vivem em regiões frias) ou comportamentais (como padrões de migração de aves).
+A seleção natural não é um processo planejado ou direcionado. Ela ocorre de maneira gradual e não-intencional, com pequenas mudanças acumulando-se ao longo de gerações. Essa acumulação pode eventualmente levar ao surgimento de novas espécies, um processo conhecido como especiação.
+Abaixo vemos uma ilustração das principais espécies que foram responsáveis pela transição de animais aquáticos até os terrestres:
+
+Instruções: Você pode tocar no botão “Pressione para avançar” e verá a imagem atual do animal evoluindo para outro animal, exibindo a cada vez uma espécie mais recente da transição de aquáticos para terrestres. Você verá que o conteúdo abaixo do animal também irá mudar. Ao chegar no último animal, é possível tocar em “Pressione para reiniciar”, para que volte ao primeiro animal.
+        ]],
+        x = 370,
+        y = title.y + 210,
+        width = 695,
+        font = native.systemFontRegular,
+        fontSize = 12,
+        align = "justify"
     })
 
     local nextButton = display.newImage(
@@ -68,6 +94,8 @@ function scene:create(event)
     end)
 
     objects:insert(title)
+    objects:insert(content)
+    objects:insert(pageNumber)
     objects:insert(soundControlGroup)
     objects:insert(nextButton)
     objects:insert(previousButton)
