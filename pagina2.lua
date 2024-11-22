2a8026a4ecaa0d11fac8e7cbcabfc5ec4c99efd0
Rafael Souza <rafaeljosegomes25@gmail.com>
2024-11-22 19:36:58 -0300

    pagina2: add base components

--- pagina2.lua
+++ pagina2.lua
@@ -1,6 +1,38 @@
 local composer = require "composer"
+local SoundControl = require "components.SoundControl"
 
 local scene = composer.newScene()
+local capaSound = audio.loadSound("pagina2.mp3")
+local soundChannel
+local isSoundOn = false
+
+local function playOrRestartSound()
+    if soundChannel and audio.isChannelActive(soundChannel) then
+        audio.stop(soundChannel)
+    end
+    soundChannel = audio.play(capaSound)
+end
+
+local function pauseSound()
+    if soundChannel and audio.isChannelActive(soundChannel) then
+        audio.pause(soundChannel)
+    end
+end
+
+local function resumeSound()
+    if soundChannel and audio.isChannelPaused(soundChannel) then
+        audio.resume(soundChannel)
+    else
+        playOrRestartSound()
+    end
+end
+
+local soundControlGroup = SoundControl.new({
+    isSoundOn = isSoundOn,
+    onPressButtonCB = playOrRestartSound,
+    pauseSound = pauseSound,
+    resumeSound = resumeSound
+})
 
 function scene:create(event)
     local objects = self.view
@@ -26,18 +58,61 @@ function scene:create(event)
     )
 
     nextButton:addEventListener("tap", function()
+        pauseSound()
         composer.gotoScene("pagina3", "fade")
     end)
 
     previousButton:addEventListener("tap", function()
+        pauseSound()
         composer.gotoScene("capa", "fade")
     end)
 
     objects:insert(title)
+    objects:insert(soundControlGroup)
     objects:insert(nextButton)
     objects:insert(previousButton)
 end
 
+function scene:show(event)
+    local phase = event.phase
+    local sceneGroup = self.view
+
+    if phase == "will" then
+        if soundControlGroup then
+            soundControlGroup:removeSelf()
+            soundControlGroup = nil
+        end
+
+        soundControlGroup = SoundControl.new({
+            isSoundOn = isSoundOn,
+            onPressButtonCB = playOrRestartSound,
+            pauseSound = pauseSound,
+            resumeSound = resumeSound
+        })
+
+        sceneGroup:insert(soundControlGroup)
+    end
+end
+
+function scene:destroy(event)
+    if soundControlGroup then
+        soundControlGroup:removeSelf()
+        soundControlGroup = nil
+    end
+
+    if soundChannel then
+        audio.stop(soundChannel)
+        soundChannel = nil
+    end
+
+    if capaSound then
+        audio.dispose(capaSound)
+        capaSound = nil
+    end
+end
+
 scene:addEventListener("create", scene)
+scene:addEventListener("show", scene)
+scene:addEventListener("destroy", scene)
 
 return scene
